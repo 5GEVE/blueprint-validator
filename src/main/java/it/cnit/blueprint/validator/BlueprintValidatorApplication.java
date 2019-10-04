ff0bc0f3ee3c037daa1fb8572be9c7831ab1a82b
Matteo Pergolesi <matpergo@gmail.com>
2019-10-04 16:40:23 +0200

    Initialize spring project.

---
+++ src/main/java/it/cnit/blueprint/validator/BlueprintValidatorApplication.java
@@ -0,0 +1,13 @@
+package it.cnit.blueprint.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprintValidatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlueprintValidatorApplication.class, args);
+  }
+
+}
